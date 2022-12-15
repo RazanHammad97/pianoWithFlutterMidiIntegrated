a18731e79addb8a4d65d4ccb100a9788bfee0b23
RazanHammad97 <razanhammadq@gmail.com>
2022-12-15 11:24:49 +0200

    Add piano with flutter midi

---
+++ android/app/src/main/kotlin/com/example/pianowithmidi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pianowithmidi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:audioplayers/audioplayers.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:piano/piano.dart';
+import 'package:flutter_midi/flutter_midi.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+ FlutterMidi fluttermidi = new FlutterMidi();
+  @override
+  void initState() {
+    load('assets/Best of Guitars-4U-v1.0.sf2');
+    super.initState();
+  }
+
+  void load(String asset) async {
+    fluttermidi.unmute(); // Optionally Unmute
+    ByteData _byte = await rootBundle.load(asset);
+    fluttermidi.prepare(sf2: _byte);
+  }
+  @override
+  Widget build(BuildContext context) {
+      return CupertinoApp(
+        title: 'Piano Demo',
+        home: Center(
+          child: InteractivePiano(
+            highlightedNotes: [
+              NotePosition(note: Note.C, octave: 3)
+            ],
+            naturalColor: Colors.white,
+            accidentalColor: Colors.black,
+            keyWidth: 60,
+            noteRange: NoteRange.forClefs([
+              Clef.Treble,
+            ]),
+            onNotePositionTapped: (position) {
+              // Use an audio library like flutter_midi to play the sound
+              print(position.pitch);
+              fluttermidi.playMidiNote(midi: position.pitch);
+              //load('assets/KBH-Real-Choir-V2.5.sf2');
+              // _playMusic(position.pitch);
+            },
+          ),
+        ));
+  }
+}
+
+
+
+
+//60 -- 84
